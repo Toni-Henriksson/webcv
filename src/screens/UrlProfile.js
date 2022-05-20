6788a4be320e1aab327964a9bc0d2e2084a599c0
Toni-Henriksson <henriksson.toni@outlook.com>
2022-05-20 22:09:32 +0300

    Working on backend, user specific data fetching

--- src/screens/UrlProfile.js
+++ src/screens/UrlProfile.js
@@ -1,12 +1,18 @@
-import {React} from "react";
+import {React, useEffect} from "react";
 import '../css/screens/url-profile.css';
 import NavigationBar from '../components/NavigationBar';
-import ResumeTemplate from '../components/ResumeTemplate';
-import { useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom';
+import { getDatabase, ref, set, child, get } from "firebase/database";
+
 
 const UrlProfile = () => {
     const params = useParams();
+
     // TODO: search firebase DB for user matching params name and render that to profile component
+    useEffect(() => {
+        // Search params profile form db -> save info to state -> render cv with info
+
+    },[]);
     return (
         <>
         <NavigationBar></NavigationBar>
