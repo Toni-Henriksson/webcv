054dab7bb9224f654048a542f8e09bd21f9eee59
Toni-Henriksson <henriksson.toni@outlook.com>
2022-05-21 21:22:34 +0300

    Working on backend, user specific data fetching

--- src/screens/UrlProfile.js
+++ src/screens/UrlProfile.js
@@ -5,11 +5,14 @@ import { useParams } from 'react-router-dom';
 import { getDatabase, onValue, ref } from "firebase/database";
 
 const UrlProfile = () => {
+    // GET Username from given URL 
     const params = useParams();
+    // GET UID For given username
     const [user, setUser] = useState("");
-
+    // DIY Eventlistener for when data is received from DB
+    const [dataReceived, setDataReceived] = useState(false);
     // Data received from database bassed on url username
-    const [data, setData] = useState([]);
+    const [uidData, setUidData] = useState([]);
 
     useEffect(() => {
          onValue(ref(getDatabase(),'userrouting/'+ params.username), snapshot=>{
@@ -17,27 +20,27 @@ const UrlProfile = () => {
              if(data !== null){
                 const tempUid = data.uid;
                 setUser(tempUid);
-                console.log("Users uid: " + data.uid);
-                GetData();
              }
              else{
                  console.log("No data receivsded from the backend of this user :(")
              }
+             setDataReceived(true)
          })
     },[]);
+    useEffect(() => {
+        if(dataReceived == true){
+            onValue(ref(getDatabase(), 'users/' + user), snapshot => {
+                const dataz = snapshot.val();
+                if(dataz !== null){
+                    setUidData(dataz);
+                }
+                else{
+                    console.log("No data from DB")
+                }
+            })
+        }
+   },[dataReceived]);
 
-    const GetData = () => {
-        onValue(ref(getDatabase(), 'users/' + user), snapshot => {
-            const dataz = snapshot.val();
-            if(dataz !== null){
-                setData(dataz);
-                console.log("Data received")
-            }
-            else{
-                console.log("No data from DB")
-            }
-        })
-    }
     return (
         <>
         <NavigationBar></NavigationBar>
@@ -45,8 +48,8 @@ const UrlProfile = () => {
             <h1>This is URL user specific site</h1>
             <h1>Username: {params.username}</h1>
             <h1>User UID: {user}</h1>;
-            <h1>User Email based on uid: {data.email}</h1>;
-            <h1>Skills based on uid: {data.skills}</h1>;
+            <h1>User Email based on uid: {uidData.email}</h1>;
+            <h1>Skills based on uid: {uidData.skills}</h1>;
         </div>
       </>
     );
