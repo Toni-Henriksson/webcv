14efee3f8ee656327b5a58820b81a5a26fbccf5c
Toni-Henriksson <henriksson.toni@outlook.com>
2022-05-24 08:58:12 +0300

    Carousel for resume templates

--- src/components/TemplateCarousel.js
+++ src/components/TemplateCarousel.js
@@ -19,16 +19,16 @@ const TemplateCarousel = () => {
             </div>
             <div className="carousel-content">
                 <div className="template-card">
-                    <h1>X</h1>
+                    <h1>1</h1>
                 </div>
                 <div className="template-card">
-                    <h1>X</h1>
+                    <h1>2</h1>
                 </div>
                 <div className="template-card">
-                    <h1>X</h1>
+                    <h1>3</h1>
                 </div>
                 <div className="template-card">
-                    <h1>X</h1>
+                    <h1>4</h1>
                 </div>
             </div>
             <div className="carousel-nav">
