e47c8e61b0b45a71a3aaad236e5c045e27a48672
Toni-Henriksson <henriksson.toni@outlook.com>
2022-05-24 10:54:19 +0300

    Carousel for resume templates

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,4 +1,4 @@
-import {React} from "react";
+import {React, useState} from "react";
 import '../css/screens/profile.css';
 import NavigationBar from '../components/NavigationBar';
 import ResumeTemplate from '../components/ResumeTemplate';
@@ -7,10 +7,18 @@ import { useParams } from 'react-router-dom'
 
 const Profile = () => {
     // TODO: search firebase DB for user matching params name and render that to profile component
+
+    // Current selected template
+    const [template,setTemplate] = useState(1);
+    // All avaible templates
+    const templateArray = [
+      <ResumeTemplate></ResumeTemplate>
+    ];
     return (
         <>
         <NavigationBar></NavigationBar>
         <TemplateCarousel></TemplateCarousel>
+        {/*Get stored template id and render that template from DB*/}
         <div className="profile-wrapper">
           <ResumeTemplate></ResumeTemplate>
         </div>
