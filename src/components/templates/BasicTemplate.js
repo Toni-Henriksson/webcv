d76433280498749d7ae94cff6e4b58c45b39d952
Toni-Henriksson <henriksson.toni@outlook.com>
2022-05-26 12:27:42 +0300

    Multi-page-register information -> database

--- src/components/templates/BasicTemplate.js
+++ src/components/templates/BasicTemplate.js
@@ -1,8 +1,9 @@
 import {React, useEffect, useState} from "react";
 import '../../css/components/templates/basictemplate.css';
 
-const BasicTemplate = (props) => {
+const BasicTemplate = ({ templateData }) => {
     const profileImage = require('../../images/claymonster.png');
+    // email, fullname, username, experience, education, skills, links, phoneNumber
     return (
         <div className="template-container">
             <div className="template-left-part">
@@ -12,19 +13,20 @@ const BasicTemplate = (props) => {
             <h1 className="template-left-h1">PROFILE</h1>
             <p className="template-left-p">liibalaaba</p>
             <h1 className="template-left-h1">CONTACTS</h1>
-            <p className="template-left-p">github.com/toni-henriksson</p>
+            <p className="template-left-p">{templateData.links}</p>
+            <p className="template-left-p">{templateData.phoneNumber}</p>
+            <p className="template-left-p">{templateData.email}</p>
             <h1 className="template-left-h1">REFERENCES</h1>
             <p className="template-left-p">Matti Meikäläinen</p>
             </div>
             <div className="template-right-part">
-                <h1 className="template-right-part-name">{props?.username}</h1>
-                <p className="template-right-part-title">{props?.title}</p>
+                <h1 className="template-right-part-name">{templateData.fullName}</h1>
                 <h1 className="template-h1">EDUCATION</h1>
-                <p className="template-p">{props?.education}</p>
+                <p className="template-p">{templateData.education}</p>
                 <h1 className="template-h1">EMPLOYMENT</h1>
-                <p className="template-p">{props?.employment}</p>
+                <p className="template-p">{templateData.experience}</p>
                 <h1 className="template-h1">SKILLS & EXPERTISE</h1>
-                <p className="template-p">{props?.skills}</p>
+                <p className="template-p">{templateData.skills}</p>
             </div>
         </div>    
     );
--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,4 +1,6 @@
-import {React, useState} from "react";
+import {React, useState, useEffect} from "react";
+import { getDatabase, onValue, ref } from "firebase/database";
+import { getAuth, onAuthStateChanged} from "firebase/auth";
 import '../css/screens/profile.css';
 import NavigationBar from '../components/NavigationBar';
 import ResumeTemplate from '../components/ResumeTemplate';
@@ -6,16 +8,52 @@ import BasicTemplate from '../components/templates/BasicTemplate'
 import TemplateCarousel from "../components/TemplateCarousel";
 
 const Profile = () => {
-    // TODO: search firebase DB for user matching params name and render that to profile component
 
     // Current selected template
     const [template,setTemplate] = useState(1);
+    const author = getAuth();
+
+    // Info from DB -> This dataform -> As props to template component -> render to screen
+    const [templateData, setTemplateData] = useState({
+      email: "",
+      fullName: "",
+      username: "",
+      experience: "",
+      education: "",
+      skills: "",
+      links: "",
+      phoneNumber: "",
+    });
 
     // All avaible templates basic, modern, retro, simple
     const templateArray = [
       <ResumeTemplate></ResumeTemplate>,
-      <BasicTemplate></BasicTemplate>
+      <BasicTemplate templateData={templateData}></BasicTemplate>
     ];
+    
+    useEffect(() => {
+        // Gets data from user-specific db and stores it to setData state. (render them to screen after)
+        onAuthStateChanged(author, (user) => {
+            if (user) {
+              // User is signed in
+              const uid = user.uid;
+              onValue(ref(getDatabase(), 'users/' + uid), snapshot => {
+                const data = snapshot.val();
+                if(data !== null){
+                    setTemplateData(data)
+                }
+                else{
+                    console.log("No data from DB")
+                }
+            })
+              // ...
+            } else {
+              // User is signed out
+                console.log("User not signed in.");
+            }
+        });
+    },[]);
+
     return (
         <>
         <NavigationBar></NavigationBar>
