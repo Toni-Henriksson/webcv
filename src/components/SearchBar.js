203bc38e6f46b3b1bc6930bd30b9f119aa5e6914
Toni-Henriksson <henriksson.toni@outlook.com>
2022-06-04 23:29:43 +0300

    Fixing bugs...refactoring...

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,10 +2,12 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const SearchBar = () => {
+    const searchIcon = require('../images/search.png');
     const [search, setSearch] = useState('');
     let navigation = useNavigate();
     const sub = (e) => {
         e.preventDefault();
+        // TODO: If statement that checks if route exists
         navigation("/" + search);
         setSearch('');
     }
@@ -14,7 +16,9 @@ const SearchBar = () => {
         <form onSubmit={(event) => {sub(event)}}>
             <div>
                 <input type="text" placeholder="Search profile.." className="nav-search-bar" onChange={(e)=>{setSearch(e.target.value)}} value={search}></input>
-                <button className="experience-submit-btn">Search</button>
+                <button className="search-submit-btn">
+                    <img src={searchIcon} alt="Logo" width="100%" height="100%" overlay="cover"></img>
+                </button>
             </div>
         </form>
     </div>
