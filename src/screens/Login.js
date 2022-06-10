957d6af2ccbb9f33f32a89e2abc75a4d5c728694
Toni-Henriksson <henriksson.toni@outlook.com>
2022-06-10 21:53:38 +0300

    Fixed several user routing bugs

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -3,6 +3,7 @@ import '../css/screens/login.css';
 import NavigationBar from '../components/NavigationBar';
 import { login } from '../backend/firebase-utility';
 import { useNavigate } from "react-router-dom";
+import background from "../images/bg2.png";
 
 const Login = () => {
     const [loginEmail, setLoginEmail] = useState("");
@@ -17,9 +18,13 @@ const Login = () => {
         <>
         <NavigationBar></NavigationBar>
         <div className="wrapper-content-login">
-            <input className="login-input" placeholder="Email address" onChange={ (event) => {setLoginEmail(event.target.value)} }></input>
-            <input className="login-input" placeholder="Password" onChange={ (event) => {SetLoginPassword(event.target.value)} }></input>
-            <button className="login-btn-01" onClick={handleLogin}>Login</button>
+            <div className="login-panel" style={{backgroundImage: `url(${background})`, backgroundRepeat: 'no-repeat', objectFit: 'fill'}}>
+                <div className="login-items">
+                    <input className="login-input" placeholder="Email address" onChange={ (event) => {setLoginEmail(event.target.value)} }></input>
+                    <input className="login-input" placeholder="Password" onChange={ (event) => {SetLoginPassword(event.target.value)} }></input>
+                    <button className="login-btn-01" onClick={handleLogin}>Login</button>
+                </div>
+            </div>
         </div>
         </>
     );
